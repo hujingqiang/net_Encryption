7a3c4e14f91e8ea8650f38f2973d9a64011c27d9
hujingqiang <hujingqiang@yahoo.com>
2017-07-02 23:24:40 +0800

    test update

--- testConsole20170619/Program.cs
+++ testConsole20170619/Program.cs
@@ -7,14 +7,15 @@ namespace testConsole20170619
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World again!");
 
 			CryptLib _crypt = new CryptLib();
 			string plainText = "This is the text to be encrypted.";
             String iv = "4NAfcTL5nWERGSLl";//CryptLib.GenerateRandomIV(16); //16 bytes = 128 bits
 			string key = CryptLib.getHashSha256("my secret key", 32); //32 bytes = 256 bits
 			String cypherText = _crypt.encrypt(plainText, key, iv);
-			Console.WriteLine("iv=" + iv);
+			Console.WriteLine("isv=" + iv);
 			Console.WriteLine("key=" + key);
 			Console.WriteLine("Cypher text=" + cypherText);
 			Console.WriteLine("Plain text =" + _crypt.decrypt(cypherText, key, iv));
